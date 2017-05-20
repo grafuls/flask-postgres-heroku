c2ca4233712646c947f99f49fa35e48346799b7a
Gonzalo Rafuls <grafuls@gmail.com>
2017-05-20 19:13:57 +0200

    Added Ledger and Factors

--- app.py
+++ app.py
@@ -31,12 +31,60 @@ class History(db.Model):
     __tablename__ = "history"
     id = db.Column(db.Integer, primary_key=True)
     currency = db.Column(db.String(8))
-    value = db.Column(db.Numeric(10,5))
+    value = db.Column(db.Numeric(10, 5))
     timestamp = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, currency, value):
         self.currency = currency
         self.value = value
+
+
+class Ledger(db.Model):
+    __tablename__ = "ledger"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(250))
+    usd = db.Column(db.Numeric(10, 5))
+    xbt = db.Column(db.Numeric(10, 5))
+    eth = db.Column(db.Numeric(10, 5))
+    rate = db.Column(db.Numeric(10, 5))
+    commision = db.Column(db.Numeric(10, 5))
+    direction = db.Column(db.String(8))
+
+    def __init__(
+            self,
+            description=None,
+            usd=0,
+            xbt=0,
+            eth=0,
+            rate=0,
+            direction=None):
+        self.description = description
+        self.usd = usd
+        self.xbt = xbt
+        self.eth = eth
+        self.rate = rate
+        self.commision = commision
+        self.direction = direction
+
+
+class Factors(db.Model):
+    __tablename__ = "factors"
+    id = db.Column(db.Integer, primary_key=True)
+    usd = db.Column(db.Numeric(10, 5))
+    xbt = db.Column(db.Numeric(10, 5))
+    eth = db.Column(db.Numeric(10, 5))
+    ledger_id = db.Column(db.Integer, ForeignKey("ledger.id"), nullable=False)
+
+    def __init__(
+            self,
+            usd=0,
+            xbt=0,
+            eth=0,
+            ledger_id):
+        self.usd = usd
+        self.xbt = xbt
+        self.eth = eth
+        self.ledger_id = ledger_id
 
 
 # Set "homepage" to index.html
