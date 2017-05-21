7e9e9b3580da17dd5ba5f47ad63169983419c836
Gonzalo Rafuls <grafuls@gmail.com>
2017-05-21 23:24:19 +0200

    Fixed celery decorator

--- tasks.py
+++ tasks.py
@@ -8,7 +8,7 @@ celery.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                    CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 
-@app.task
+@celery.task
 def add(x, y):
     app.logger.info('Executing add function for %s*%s' % (x, y))
     return x + y
