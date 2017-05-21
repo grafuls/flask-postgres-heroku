eae7934012fdab74b61088c3b9cafb61731e4902
Gonzalo Rafuls <grafuls@gmail.com>
2017-05-21 23:21:13 +0200

    Added logger

--- tasks.py
+++ tasks.py
@@ -5,9 +5,10 @@ import os
 
 celery = Celery(app.name)
 celery.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+                   CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 
 @app.task
 def add(x, y):
+    app.logger.info('Executing add function for %s*%s' % (x, y))
     return x + y
