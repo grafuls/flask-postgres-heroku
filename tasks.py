da2a3a8310590d2fabd0eb3de37e8afe07b1d358
Gonzalo Rafuls <grafuls@redhat.com>
2017-05-23 09:33:26 +0200

    Fixed tasks
    Change-Id: I2388e678125a4e2d14c74c24ac9e6c38d471a302

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 from app import db, krapi
 from celery import Celery
 from datetime import timedelta
+from decimal import Decimal
 from models import Factors, History, Ledger
+from sqlalchemy import desc
 
 import os
 
@@ -25,20 +27,20 @@ def shakeThatMoneyMaker():
     xbt_drop = False
     eth_drop = False
 
-    latest = db.session.query(History).order_by(History.id).limit(6)
-    xbt_latest = [x.value for x in latest if x.currency == "XBTCZUSD"]
+    latest = db.session.query(History).order_by(desc(History.id)).limit(6)
+    xbt_latest = [x.value for x in latest if x.currency == XBT_PAIR]
     xbt_average = sum(xbt_latest)/len(xbt_latest)
-    eth_latest = [x.value for x in latest if x.currency == "XETHZUSD"]
+    eth_latest = [x.value for x in latest if x.currency == ETH_PAIR]
     eth_average = sum(eth_latest)/len(eth_latest)
 
     ticker = krapi.query_public('Ticker', {'pair': PAIR})
     xbt_price = ticker['result'][XBT_PAIR]['a'][0]
     eth_price = ticker['result'][ETH_PAIR]['a'][0]
 
-    xbt_drop = xbt_price < xbt_average
-    eth_drop = eth_price < eth_average
+    xbt_drop = Decimal(xbt_price) < xbt_average
+    eth_drop = Decimal(eth_price) < eth_average
 
-    balance = db.session.query(Ledger).order_by(Ledger.id).limit(1)[0]
+    balance = db.session.query(Ledger).order_by(desc(Ledger.id)).limit(1)[0]
 
     xbt = Money("xbt", 0, xbt_price)
     eth = Money("eth", 0, eth_price)
@@ -46,20 +48,20 @@ def shakeThatMoneyMaker():
     if balance.usd > 0:
         split = balance.usd/2
         source = Money("usd", split, 1)
-        _buy(source, xbt, balance)
-        _buy(source, eth, balance)
+        _buy(source, xbt)
+        _buy(source, eth)
         return
 
     if xbt_drop and not eth_drop:
         if balance.xbt > 0:
             source = Money("xbt", balance.xbt, xbt_price)
-            _buy(source, eth, balance)
+            _buy(source, eth)
         return
 
     if eth_drop and not xbt_drop:
         if balance.eth > 0:
             source = Money("eth", balance.eth, eth_price)
-            _buy(source, xbt, balance)
+            _buy(source, xbt)
         return
 
     return
@@ -68,11 +70,12 @@ def shakeThatMoneyMaker():
 class Money():
     def __init__(self, currency, amount, usd_rate):
         self.currency = currency
-        self.amount = amount
-        self.usd_rate = usd_rate
+        self.amount = Decimal(amount)
+        self.usd_rate = Decimal(usd_rate)
 
 
-def _buy(source, destination, balance):
+def _buy(source, destination):
+    balance = db.session.query(Ledger).order_by(desc(Ledger.id)).limit(1)[0]
     ledger = Ledger(rate=destination.usd_rate)
     conversion = convert(source, destination)
     factors_usd = 0
@@ -97,11 +100,12 @@ def _buy(source, destination, balance):
             if source.currency == "usd":
                 ledger.usd = balance.usd - source.amount
                 factors_usd = - source.amount
-                ledger.eth = balance.eth
+                ledger.xbt = balance.xbt
             else:
                 ledger.usd = balance.usd
                 ledger.xbt = balance.xbt - source.amount
                 factors_xbt = - source.amount
+
     db.session.add(ledger)
     db.session.commit()
     db.session.refresh(ledger)
@@ -114,7 +118,7 @@ def _buy(source, destination, balance):
 def convert(source, destination):
     pair = "ETHXBT"
     ticker = krapi.query_public('Ticker', {'pair': pair})
-    rate = ticker['result'][pair]['a'][0]
+    rate = ticker['result']['XETHXXBT']['a'][0]
     total = 0
     if source.currency == "eth":
         total = source.amount * rate
